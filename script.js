bc2c93607dd6bd9e57eb96d32ef3705006760b41
Bendegúz Székács <bendeguz.szekacs@gmail.com>
2023-10-03 20:29:58 +0200

    a

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let min = 0;
 let sec = 0;
-let delays = [4000, 2000, 1000, 500, 50];
+let delays = [2000, 1500, 1000, 500, 50, 10];
+let normal = 2;
 let speed = 2;
 let interval;
 let isRunning = false;
@@ -19,63 +20,113 @@ function updateTimer() {
     let secString = String(sec).padStart(2, "0");
     document.getElementById("min").textContent = minString;
     document.getElementById("sec").textContent = secString;
-    if (min === 19 && sec === 56) clearInterval(interval);
-    if (min === 20 && sec === 23) clearInterval(interval);
-    if (min === 45 && sec === 0) clearInterval(interval);
+    if ((min === 19 && sec === 56) || (min === 20 && sec === 23) || (min === 45 && sec === 0)) {
+        clearInterval(interval);
+        isRunning = false;
+    }
 }
+
+//#region functions
+
+function pause() {
+    if (isRunning) {
+        clearInterval(interval);
+        isRunning = false;
+    } else {
+        interval = setInterval(updateTimer, delays[speed]);
+        isRunning = true;
+    }
+}
+
+function increaseSpeed() {
+    speed++;
+    if (speed === 5) {
+        speed = 4;
+        return;
+    }
+    if (isRunning) {
+        clearInterval(interval);
+        interval = setInterval(updateTimer, delays[speed]);
+    }
+}
+
+function decreaseSpeed() {
+    speed--;
+    if (speed === -1) {
+        speed = 0;
+        return;
+    }
+    if (isRunning) {
+        clearInterval(interval);
+        interval = setInterval(updateTimer, delays[speed]);
+    }
+}
+
+function normalize() {
+    speed = normal;
+    if (isRunning) {
+        clearInterval(interval);
+        interval = setInterval(updateTimer, delays[speed]);
+    }
+}
+
+function reset() {
+    if (isRunning) clearInterval(interval);
+    min = 0;
+    sec = 0;
+    speed = normal;
+    isRunning = false;
+    document.getElementById("min").textContent = "00";
+    document.getElementById("sec").textContent = "00";
+}
+
+function fastForward() {
+    if (isRunning) {
+        clearInterval(interval);
+        let remainder;
+        if (min < 19 || (min === 19 && sec < 56)) {
+            remainder = (19 - min) * 60 + 56 - sec;
+            console.log("1: " + remainder)
+        } else if (min < 20 || (min === 20 && sec < 23)) {
+            remainder = (20 - min) * 60 + 23 - sec;
+            console.log("2: " + remainder)
+        } else {
+            remainder = (45 - min) * 60 - sec;
+            console.log("3: " + remainder)
+        }
+        let fFSpeed = (7000/remainder);
+        if (remainder < 7) fFSpeed = delays[normal];
+        console.log(fFSpeed);
+        interval = setInterval(updateTimer, fFSpeed)
+    }
+}
+
+//#endregion
 
 document.onkeyup = function (e) {
     switch (e.code) {
         case "Space":
-            if (isRunning) {
-                clearInterval(interval);
-                isRunning = false;
-            } else {
-                interval = setInterval(updateTimer, delays[speed]);
-                isRunning = true;
-            }
+            pause();
             break;
 
         case "ArrowRight":
-            speed++;
-            if (speed === 5) {
-                speed = 4;
-                break;
-            }
-            if (isRunning) {
-                clearInterval(interval);
-                interval = setInterval(updateTimer, delays[speed]);
-            }
+            increaseSpeed();
             break;
 
         case "ArrowLeft":
-            speed--;
-            if (speed === -1) {
-                speed = 0;
-                break;
-            }
-            if (isRunning) {
-                clearInterval(interval);
-                interval = setInterval(updateTimer, delays[speed]);
-            }
+            decreaseSpeed();
             break;
 
         case "KeyN":
-            speed = 2;
-            if (isRunning) {
-                clearInterval(interval);
-                interval = setInterval(updateTimer, delays[speed]);
-            }
+            normalize();
             break;
 
         case "KeyB":
-            if (isRunning) clearInterval(interval);
-            min = 0;
-            sec = 0;
-            speed = 2;
-            isRunning = false;
-            document.getElementById("min").textContent = "00";
-            document.getElementById("sec").textContent = "00";
+            reset();
+            break;
+
+        case "KeyT":
+            fastForward();
             break;
 
         default:
