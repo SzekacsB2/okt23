8bddcb250d532711dcec52bdc50c531f1f7a94c0
Bendegúz Székács <bendeguz.szekacs@gmail.com>
2023-10-06 19:39:22 +0200

    a

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+const normalSpeeds = [1000, 7000, 700];
+const speeds = [10, 100, 1000, 1000, 1000, 1000, 1000, 1000, 1000];
+const fastForwardTime = 7000;
+
 let min = 0;
 let sec = 0;
-let delays = [2000, 1500, 1000, 500, 50, 10];
-let normal = 2;
-let speed = 2;
 let interval;
 let isRunning = false;
+let normalIndex = 0;
 
 function updateTimer() {
     sec++;
@@ -15,58 +17,54 @@ function updateTimer() {
             min = 0;
         }
     }
-
-    let minString = String(min).padStart(2, "0");
-    let secString = String(sec).padStart(2, "0");
-    document.getElementById("min").textContent = minString;
-    document.getElementById("sec").textContent = secString;
-    if ((min === 19 && sec === 56) || (min === 20 && sec === 23) || (min === 45 && sec === 0)) {
-        clearInterval(interval);
-        isRunning = false;
+    document.getElementById("min").textContent = String(min).padStart(2, "0");
+    document.getElementById("sec").textContent = String(sec).padStart(2, "0");
+    if (min === 19 && sec === 56) {
+        stopTimer(1, "\nP1");
+    }
+    if (min === 20 && sec === 23) {
+        stopTimer(2, "\nP2");
+    }
+    if (min === 45 && sec === 0) {
+        stopTimer(0, "\nP3");
     }
 }
 
 //#region functions
 
-function pause() {
+function stopTimer(newIndex, message) {
+    clearInterval(interval);
+    isRunning = false;
+    normalIndex = newIndex;
+    console.log(message);
+}
+
+function toggleTimer() {
     if (isRunning) {
-        clearInterval(interval);
-        isRunning = false;
+        stopTimer(normalIndex, "\nP")
     } else {
-        interval = setInterval(updateTimer, delays[speed]);
+        console.log("\nUP " + min + ":" + sec);
+        console.log(normalSpeeds[normalIndex]);
+        interval = setInterval(updateTimer, normalSpeeds[normalIndex]);
         isRunning = true;
     }
 }
 
-function increaseSpeed() {
-    speed++;
-    if (speed === 5) {
-        speed = 4;
-        return;
-    }
+function changeSpeed(n) {
     if (isRunning) {
         clearInterval(interval);
-        interval = setInterval(updateTimer, delays[speed]);
+        console.log("\nC " + min + ":" + sec);
+        console.log(speeds[n]);
+        interval = setInterval(updateTimer, speeds[n]);
     }
 }
 
-function decreaseSpeed() {
-    speed--;
-    if (speed === -1) {
-        speed = 0;
-        return;
-    }
+function normalSpeed() {
     if (isRunning) {
         clearInterval(interval);
-        interval = setInterval(updateTimer, delays[speed]);
-    }
-}
-
-function normalize() {
-    speed = normal;
-    if (isRunning) {
-        clearInterval(interval);
-        interval = setInterval(updateTimer, delays[speed]);
+        console.log("\nN " + min + ":" + sec);
+        console.log(normalSpeeds[normalIndex]);
+        interval = setInterval(updateTimer, normalSpeeds[normalIndex]);
     }
 }
 
@@ -74,8 +72,9 @@ function reset() {
     if (isRunning) clearInterval(interval);
     min = 0;
     sec = 0;
-    speed = normal;
+    normalIndex = 0;
     isRunning = false;
+    console.log("\nR");
     document.getElementById("min").textContent = "00";
     document.getElementById("sec").textContent = "00";
 }
@@ -83,53 +82,50 @@ function reset() {
 function fastForward() {
     if (isRunning) {
         clearInterval(interval);
+        console.log("\nF " + min + ":" + sec);
         let remainder;
         if (min < 19 || (min === 19 && sec < 56)) {
             remainder = (19 - min) * 60 + 56 - sec;
-            console.log("1: " + remainder)
+            console.log("1 " + remainder)
         } else if (min < 20 || (min === 20 && sec < 23)) {
             remainder = (20 - min) * 60 + 23 - sec;
-            console.log("2: " + remainder)
+            console.log("2 " + remainder)
         } else {
             remainder = (45 - min) * 60 - sec;
-            console.log("3: " + remainder)
+            console.log("3 " + remainder)
         }
-        let fFSpeed = (7000/remainder);
-        if (remainder < 7) fFSpeed = delays[normal];
-        console.log(fFSpeed);
-        interval = setInterval(updateTimer, fFSpeed)
+        let fastForwardSpeed = (fastForwardTime / remainder);
+        console.log(fastForwardSpeed);
+        interval = setInterval(updateTimer, fastForwardSpeed)
     }
 }
 
 //#endregion
 
 document.onkeyup = function (e) {
-    switch (e.code) {
-        case "Space":
-            pause();
-            break;
+    if (e.code.includes('Digit')) {
+        let n = parseInt(e.code[5]) - 1;
+        changeSpeed(n);
+    } else {
+        switch (e.code) {
+            case "Space":
+                toggleTimer();
+                break;
 
-        case "ArrowRight":
-            increaseSpeed();
-            break;
+            case "KeyN":
+                normalSpeed();
+                break;
 
-        case "ArrowLeft":
-            decreaseSpeed();
-            break;
+            case "KeyR":
+                reset();
+                break;
 
-        case "KeyN":
-            normalize();
-            break;
+            case "KeyF":
+                fastForward();
+                break;
 
-        case "KeyB":
-            reset();
-            break;
-
-        case "KeyT":
-            fastForward();
-            break;
-
-        default:
-            break;
+            default:
+                break;
+        }
     }
 };
